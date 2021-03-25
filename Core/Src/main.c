b4ffaddc889943e7c5ebad2767e9a3e41559db8f
Pawarich Ngamvuttivorn <42509276+use555555@users.noreply.github.com>
2021-03-25 13:59:39 +0700

    Test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -43,7 +43,7 @@
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+uint32_t TimeStamp = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -96,6 +96,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  if(HAL_GetTick() - TimeStamp >= 100)
+	  {
+		  TimeStamp = HAL_GetTick();
+		  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	  }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
